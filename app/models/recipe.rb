08ae65d72a385e2f2be14b291cf64b3bcc17c7a3
sheridan <sheridanmaustad@gmail.com>
2022-05-02 18:24:34 -0600

    commit tree dca23dcfb2287703bd466314ebc513a2eed710d1

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
+# class Recipe < ApplicationRecord
+#   belongs_to :user
 
-  validates :title, presence: true
-  validates :instructions, length: { minimum: 50 }
-end
+#   validates :title, presence: true
+#   validates :instructions, length: { minimum: 50 }
+# end
